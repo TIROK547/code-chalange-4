3eeb9770d3668f5996e182ab75842cee58354f7e
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-03-07 12:59:44 +0330

    M's first commit

--- AliBaba/Move/models.py
+++ AliBaba/Move/models.py
@@ -5,7 +5,19 @@ class Passenger(models.Model):
     name = models.CharField(max_length=20)
     
     
-class Airplane(models.Model):
+class Airplane(models.Model):pass
 
 
-class 
+class Info(models.Model):
+    city = models.CharField(max_length=100)
+    reservation = models.ForeignKey(to= Reservation)
+    date = models.DateTimeField()
+
+
+class Bus(models.Model):
+    passenger = models.ForeignKey(to= Passenger, on_delete= models.CASCADE)
+
+
+class Train(models.Model):
+    Passenger = models.ForeignKey(to= Passenger, on_delete= models.CASCADE)
+
