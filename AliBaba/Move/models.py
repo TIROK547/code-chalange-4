d5cbe05c2de69f35f5fa5522f4dab541df8765a2
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-03-07 12:41:46 +0330

    project started

--- AliBaba/Move/models.py
+++ AliBaba/Move/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Passenger(models.Model):
+    name = models.CharField(max_length=20)
+    
+    
+class Airplane(models.Model):
+
+
+class 
